09b3d6159cb6d97422ef031772b403e4d4e29978
cyrinefakhfakh <sirinefakhfakh03@gmail.com>
2024-01-22 20:33:22 +0100

    second

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// MainActivity.java
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonNavigate = findViewById(R.id.buttonNavigate);
+        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the SecondActivity
+                Intent intent = new Intent(MainActivity.this, android.R.layout.class);
+                startActivity(intent);
+            }
+        });
     }
 }
