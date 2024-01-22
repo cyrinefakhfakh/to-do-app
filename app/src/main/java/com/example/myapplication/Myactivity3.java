90b11bf3f9196a988b712ce1fe89e53759b83298
cyrinefakhfakh <sirinefakhfakh03@gmail.com>
2024-01-22 22:33:38 +0100

    third

---
+++ app/src/main/java/com/example/myapplication/Myactivity3.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Myactivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myactivity3);
+    }
+}
